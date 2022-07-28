cbeaf504867abfb4f3603ff0192933f25a5fb7e8
Robert C. Bowman III <bowman@opuscule.com>
2022-07-28 07:12:35 -0400

    Initial Commit

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+//const eleventyPluginFilesMinifier = require("@sherby/eleventy-plugin-files-minifier");
+
+module.exports = function(eleventyConfig) {
+	// eleventyConfig.addPassthroughCopy('./src/images');
+	// eleventyConfig.addPassthroughCopy('./src/js');
+	// eleventyConfig.addPassthroughCopy('./src/styles');
+	// eleventyConfig.addPassthroughCopy('./src/resource');
+	// eleventyConfig.addPassthroughCopy('./src/_redirects');
+	//eleventyConfig.addPlugin(eleventyPluginFilesMinifier);
+	return {
+		dir: {
+			input:"src",
+			output:"public"
+		},
+		passthroughFileCopy: true
+	};	
+}
